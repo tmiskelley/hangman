d02df60924f4382193cabef219d2488e30642ba9
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-02-26 12:52:58 -0500

    Implemented load functionality

--- application.rb
+++ application.rb
@@ -11,21 +11,25 @@ end
 # Models a hangman game instance
 class Hangman
   def initialize(word)
-    puts "Welcome to Hangman!\nEnter 'quit' to quit at any time \n\nTry to guess the word: "
+    puts "Welcome to Hangman!\nEnter 'quit' to quit at any time"
 
-    @new_word = word
+    if File.exist?('save-data.json')
+      new_game(word) unless load_game?
+    else
+      new_game(word)
+    end
     p @new_word
-    @spaces = Array.new(@new_word.length) { nil }
-    @wrong_guesses = []
-    @max_error = 6
     play
   end
 
   protected
 
   def play
+    print_wrong
+    puts 'Try to guess the word:'
     @spaces.each do |element|
-      print '_ ' if element.nil?
+      item = element.nil? ? '_ ' : "#{element} "
+      print item
     end
     print "\n"
     main_loop
@@ -34,6 +38,36 @@ class Hangman
 
   private
 
+  def new_game(word)
+    @new_word = word
+    @spaces = Array.new(@new_word.length) { nil }
+    @wrong_guesses = []
+    @max_error = 6
+  end
+
+  def load_game?
+    print 'Would you like to load your previous save? y/n: '
+    choice = gets.chomp
+    case choice
+    when 'y'
+      load_game
+      true
+    when 'n'
+      false
+    else
+      puts 'Invalid input.'
+      load_game?
+    end
+  end
+
+  def load_game
+    data = JSON.parse(File.read('save-data.json'))
+    @new_word = data['new_word']
+    @spaces = data['spaces']
+    @wrong_guesses = data['wrong_guesses']
+    @max_error = data['max_error']
+  end
+
   def main_loop
     while @spaces.any?(&:nil?)
       input = user_input
@@ -41,8 +75,7 @@ class Hangman
       wrong_letter(input) unless @new_word.include?(input)
       break if @max_error.zero?
 
-      print 'Incorrect guesses: '
-      @wrong_guesses.each { |i| print "#{i} " }
+      print_wrong
       update_spaces(input)
     end
   end
@@ -61,7 +94,6 @@ class Hangman
   end
 
   def update_spaces(input)
-    puts "\n"
     @new_word.each_with_index do |element, i|
       @spaces[i] = input if element == input
     end
@@ -77,6 +109,12 @@ class Hangman
     @max_error -= 1
     puts "Incorrect! You have #{@max_error} lives left"
     @wrong_guesses.push(input) unless input.empty?
+  end
+
+  def print_wrong
+    print 'Incorrect guesses: '
+    @wrong_guesses.each { |i| print "#{i} " }
+    print "\n"
   end
 
   def game_over
